83078b342466039e4f47bee64c0c6d48d6e52268
milevkiril <kirilangelov.m@gmail.com>
2020-10-17 18:05:40 +0300

    First commit

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public virtual ApplicationUser User {get; set;}
+
+    }
+}
